4ec67c6d9a17ec969c1c8b27e9aaf03f9621cb31
Pulok Biswas <pulokbiswas199@gmail.com>
2021-12-25 16:44:20 +0600

    my project

---
+++ app/src/main/java/com/samulitfirstproject/supply365/model/saveKeyList.java
@@ -0,0 +1,21 @@
+package com.samulitfirstproject.supply365.model;
+
+public class saveKeyList {
+    private String key;
+
+    public saveKeyList(){
+
+    }
+
+    public saveKeyList(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
